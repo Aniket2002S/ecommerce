0568592f6fc7c0905dc4880b1057664b67d465eb
Aniket Sonawane <88265544+Aniket2002S@users.noreply.github.com>
2023-12-18 20:14:57 +0530

    Update FirebaseConfig.jsx

--- vite-project/src/fireabase/FirebaseConfig.jsx
+++ vite-project/src/fireabase/FirebaseConfig.jsx
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCS1kX4TchYIJ1fwI8ju3iOu_20PCMxFDE",
-  authDomain: "myfirst-bed83.firebaseapp.com",
-  projectId: "myfirst-bed83",
-  storageBucket: "myfirst-bed83.appspot.com",
-  messagingSenderId: "910064312212",
-  appId: "1:910064312212:web:65b89854bf278db2cbf981"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
